e8642698c8eca38e962e1e3bf2b3cbde1852cca5
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2015-05-14 16:18:01 +0100

    fix

--- Overthrow/Assets/Code/PlayerControl.cs
+++ Overthrow/Assets/Code/PlayerControl.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+enum Mode {ARPG, Stealth};
+
 public class PlayerControl : MonoBehaviour {
 	
 	public float speed = 3.0f;
+	public float blinkCooldown = 15;
+	public float naturesWrathCooldown = 15;
+	private float blinkTimeSpan;
+	private float naturesWrathTimeSpan;
+
 	private Vector3 targetPosition, cameraTargetPosition;
+
 	public bool moving = false;
+
 	private Ray ray;
+
 	private RaycastHit hit;
-	private Transform currentTarget;
+	
 	public Transform elementalMissiles;
-	private float blinkCooldown;
+	private Transform currentTarget;
+
+	private Mode mode;
 
 	// Use this for initialization
 	void Start () {
-		blinkCooldown = Time.time;
+		blinkTimeSpan = Time.time;
+		mode = Mode.ARPG;
 	}
 	
 	// Update is called once per frame
@@ -44,21 +57,18 @@ public class PlayerControl : MonoBehaviour {
 
 	void PlayerSkills() {
 		if (Input.GetKeyDown(KeyCode.Alpha1)) {
-			if (currentTarget.tag == "Enemy") {
-				elementalMissiles.GetComponent<MissileMovement>().target = currentTarget;
-				elementalMissiles.position = new Vector3(transform.position.x+1,transform.position.y,transform.position.z+1);
-				Debug.Log (currentTarget.name);
-				Instantiate(elementalMissiles);
+			if (mode == Mode.ARPG) {
+				ElementalMissiles();
+			} else {
 			}
 		}
 		
-		
 		if (Input.GetKeyDown(KeyCode.Alpha2)) {
-			if (blinkCooldown <= Time.time) {
-				moving = false;
-				GetMouseWorldPosition();
-				transform.position = targetPosition;
-				blinkCooldown = Time.time + 15f;
+			if (mode == Mode.ARPG) {
+				if (blinkTimeSpan <= Time.time) {
+					Blink ();
+				}
+			} else {
 			}
 		}
 		
@@ -69,6 +79,31 @@ public class PlayerControl : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Alpha4)) {
 			Debug.Log ("Pressed key 4!");
 		}
+
+		if (Input.GetMouseButtonDown (1)) {
+			Debug.Log ("Switching mode!");
+			if (mode == Mode.ARPG) {
+				mode = Mode.Stealth;
+			} else {
+				mode = Mode.ARPG;
+			}
+		}
+	}
+
+	void ElementalMissiles() {
+		if (currentTarget.tag == "Enemy") {
+			elementalMissiles.GetComponent<MissileMovement>().target = currentTarget;
+			elementalMissiles.position = new Vector3(transform.position.x+1,transform.position.y,transform.position.z+1);
+			Debug.Log (currentTarget.name);
+			Instantiate(elementalMissiles);
+		}
+	}
+
+	void Blink() {
+		moving = false;
+		GetMouseWorldPosition();
+		transform.position = targetPosition;
+		blinkTimeSpan = Time.time + blinkCooldown;
 	}
 
 	void MouseMovement() {
@@ -87,7 +122,7 @@ public class PlayerControl : MonoBehaviour {
 
 	void GetMouseWorldPosition() {
 		Plane playerPlane = new Plane(Vector3.up, transform.position);
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		float hitDist = 0.0f;
 		var rotation = transform.rotation;
 		
