6c9b0701c3ac9dfd357bb121984c8a95eb258d42
Pedro Tavares <p.miguel.tr@gmail.com>
2015-06-18 21:54:26 +0100

    burning damage

--- Overthrow/Assets/Code/AutoDestroyParticleSystem.cs
+++ Overthrow/Assets/Code/AutoDestroyParticleSystem.cs
@@ -3,11 +3,24 @@ using System.Collections;
 
 public class AutoDestroyParticleSystem : MonoBehaviour {
 
+	public static float BurningDuration = 0.5f;
+	public GameObject player;
+
 	public void Start(){
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	public void Update(){
 		if (Time.deltaTime > 4)
 			Destroy (gameObject);
+	}
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.gameObject == player) {
+			player.GetComponent<PlayerControl> ().StartBurning(BurningDuration);
+		}
+		//player.GetComponent<PlayerControl> ().TakeDamage (enemyDamage);
+
 	}
 }
