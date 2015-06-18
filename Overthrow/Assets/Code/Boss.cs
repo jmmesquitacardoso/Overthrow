334aeeeebf1dd6357ad4e35c41df65fe2aed92b2
daviici <davidvan93@gmail.com>
2015-06-18 17:34:49 +0100

    boss fire

--- Overthrow/Assets/Code/Boss.cs
+++ Overthrow/Assets/Code/Boss.cs
@@ -11,7 +11,6 @@ public class Boss : MonoBehaviour {
 	public int attackPower = 3000;
 	public int MaxHealth=2000;
 	public int Health{ get; private set; }
-	public Transform rain;
 	public Transform fire;
 	public Transform elementalMissiles;
 	public Transform playerTarget;
@@ -19,6 +18,9 @@ public class Boss : MonoBehaviour {
 	public float playerDistance;
 	public float fireRate = 20;
 	public float canFire;
+	private Transform fireTransform;
+	private float fireTime=2;
+	private float fireT;
 	// Use this for initialization
 	public void Awake () {
 		Health = MaxHealth;
@@ -34,14 +36,25 @@ public class Boss : MonoBehaviour {
 		if (playerDistance < 80f) {
 			lookAtPlayer ();
 		
-			if (playerDistance < 60f && playerDistance > 6f ) {
+			if (playerDistance < 60f && playerDistance > 4f ) {
 				transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
 		
-				if (playerDistance < 55f) {
+				if (playerDistance < 55f && playerDistance > 20f) {
 					if ((canFire -= Time.deltaTime) > 0)
 						return;
 					ElementalMissiles (playerTarget.position, true);
 					canFire = fireRate;
+				}
+				if(playerDistance < 20f){
+					if((fireT -= Time.deltaTime) >0)
+						return;
+					if(fireTransform != null){
+						DestroyImmediate(fireTransform.gameObject, true);
+
+					}
+					fireTransform= Instantiate (fire);
+					fireTransform.position = playerTarget.position;
+					fireT=fireTime;
 				}
 			}
 		}
